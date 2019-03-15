0cf6565c6ae56d37fd3a20253b7e6dd18c990eb2
Brian Tobal <brian.tobal@gmail.com>
2019-03-15 11:38:58 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
 
 
+
+def my_collect(array)
+  i = 0
+  collect = []
+  while i < array.length
+    collect << yield(array[i])
+    i+=1
+  end
+  collect
+end
+
+# It should take in an argument of a collection
+# Iterate over that collection using a while loop
+# execute the code in the block you call it with for each element in the collection (use the yield keyword
